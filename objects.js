276800055e900125a6acce26f1059f89790f51ef
Delanie Perez <delanieperez11@gmail.com>
2018-05-07 03:11:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,6 @@ var playlist = {artist: "object"}
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist['Slowdive'] = 'Alison'
   playlist['My Bloody Valentine'] = 'Sometimes'
+  playlist['Phil Ochs'] = "Here's to the State of Mississippi"
+  return playlist
 }
